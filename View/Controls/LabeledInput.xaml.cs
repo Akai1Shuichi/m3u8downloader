ea20caee391af02e7cc47d1a3bbf9bc3be5d9a50
toan2k1 <toan2k1window@gmail.com>
2025-09-06 03:59:44 +0700

    code base mvvm

---
+++ View/Controls/LabeledInput.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace m3u8Downloader.View.Controls
+{
+    public partial class LabeledInput : UserControl
+    {
+        public LabeledInput()
+        {
+            InitializeComponent();
+        }
+
+        public string Label
+        {
+            get { return (string)GetValue(LabelProperty); }
+            set { SetValue(LabelProperty, value); }
+        }
+
+        public static readonly DependencyProperty LabelProperty =
+            DependencyProperty.Register("Label", typeof(string), typeof(LabeledInput), new PropertyMetadata(string.Empty));
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(LabeledInput), new PropertyMetadata(string.Empty));
+    }
+}
---
+++ ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,62 @@
+﻿using m3u8Downloader.MVVM;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace m3u8Downloader.ViewModel
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+
+        private int _url;
+
+        public int Url
+        {
+            get { return _url; }
+            set { _url = value; }
+        }
+
+
+        private string _videoPath;
+        public string VideoPath
+        {
+            get => _videoPath;
+            set { _videoPath = value; OnPropertyChanged(); }
+        }
+
+        private int _maxWorker = 5;
+        public int MaxWorker
+        {
+            get => _maxWorker;
+            set { _maxWorker = value; OnPropertyChanged(); }
+        }
+
+        private string _result;
+        public string Result
+        {
+            get => _result;
+            set { _result = value; OnPropertyChanged(); }
+        }
+
+        // Commands
+        public ICommand DownloadCommand { get; }
+        public ICommand CheckSizeCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            DownloadCommand = new RelayCommand(_ => Download());
+            CheckSizeCommand = new RelayCommand(_ => CheckSize());
+        }
+
+        private void Download()
+        {
+            Result = $"Bắt đầu tải video từ: {Url}\nThư mục: {VideoPath}\nWorker: {MaxWorker}";
+        }
+
+        private void CheckSize()
+        {
+            Result = $"Đang kiểm tra size của video: {Url}";
+        }
+
+    }
+}
